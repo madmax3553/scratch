011f95c24334bed14fbe409d5e2fe42342517126
madmax3553 <jeanpaul.bouliane@gmail.com>
2024-07-06 21:38:14 -0600

    ommited

---
+++ go/bubbleTeaPro/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "github.com/charmbracelet/ssh"
+import "github.com/charmbracelet/wish"
+
+func main() {
+	fmt.Println("hello")
+	wish.WithAddress(fmt.sprintf("%s:%d", host, part)),
+}
